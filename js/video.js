6e0b7a747f0f18c885d932f0665a7c9ef3c0e005
matthewlindgren <lindmatt@umich.edu>
2024-03-31 10:35:44 -0400

    Finished?

--- js/video.js
+++ js/video.js
@@ -1,11 +1,53 @@
-var video;
+let video = document.querySelector("#player1");
+let numSlows = 0;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	video.autoplay = false;
+	video.loop = false;
+	video.load()
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	video.volume = document.querySelector("#slider").value / 100;
+	video.play();
+})
 
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+})
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	++numSlows;
+	console.log("Changed playback speed to " + video.playbackRate);
+})
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = Math.pow(0.9, numSlows-- - 1);
+	console.log("Changed playback speed to " + video.playbackRate);
+})
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime += 10;
+	if (video.currentTime >= video.duration) {
+		video.currentTime = 0;
+	}
+	console.log("Video location: " + video.currentTime);
+})
+
+document.querySelector("#mute").addEventListener("click", function() {
+	video.muted = !video.muted;
+})
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = document.querySelector("#slider").value / 100;
+	console.log("New volume: " + video.volume);
+})
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+})
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+})
